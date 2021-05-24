c778dfbeb5d2e4567285a60991dfc1909f49738c
Marcus <30908995+mickeystix@users.noreply.github.com>
2021-05-24 13:41:52 -0500

    Update main.lua

--- qb-shower/server/main.lua
+++ qb-shower/server/main.lua
@@ -9,5 +9,6 @@ AddEventHandler('qb-shower:server:washSelf', function()
     local Player = QBCore.Functions.GetPlayer(src)
 
     TriggerClientEvent('qb-shower:client:washSelf', src)
+    TriggerClientEvent('qb-shower:client:showeranim', src)
 
 end)
